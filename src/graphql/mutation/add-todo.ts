4926891f61aa3736bd849a21fb1d1d1ae20abf46
xvasat04 <xvasat04@stud.fit.vutbr.cz>
2021-12-06 15:48:29 +0100

    fix: current date in new todo

--- src/graphql/mutation/add-todo.ts
+++ src/graphql/mutation/add-todo.ts
@@ -5,14 +5,14 @@ interface Input {
 	text: string
 }
 
-const now = Date.now()
+//let now = Date.now()
 
 export default async ({ text }: Input): Promise<RequestResponse> => {
 	const db = await getDb()
 	const todo: Todo = {
 		text,
 		checked: false,
-		createdTimestamp: now
+		createdTimestamp: Date.now()
 	}
 	await db.collection('to do').insertOne(todo)
 	return {
